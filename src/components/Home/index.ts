7bb051caaf22076ea04f89a53db3005ddccd2327
pragmaticAweds <jimohafeez738@gmail.com>
2026-04-20 14:21:58 +0100

    refactor: reorder Home component exports to improve maintainability

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -1,5 +1,5 @@
-export { Header } from "./Header";
 export { Hero } from "./Hero";
+export { Header } from "./Header";
+export { Footer } from "./Footer";
 export { Solutions } from "./Solutions";
 export { HowItWorks } from "./HowItWorks";
-export { Footer } from "./Footer";
